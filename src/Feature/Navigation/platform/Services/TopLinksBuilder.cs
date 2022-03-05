4b75fa0d3d0117dd8175b270834f86ba76e5520c
Vadim Birkos <V.Birkos@sam-solutions.com>
2022-03-06 00:33:24 +0300

    change URL builder options.

--- src/Feature/Navigation/platform/Services/TopLinksBuilder.cs
+++ src/Feature/Navigation/platform/Services/TopLinksBuilder.cs
@@ -4,28 +4,30 @@ using System.Linq;
 using Mvp.Feature.Navigation.Models;
 using Sitecore.Data.Fields;
 using Sitecore.Data.Items;
+using Sitecore.Links.UrlBuilders;
 using Sitecore.Mvc.Presentation;
+using Sitecore.Resources.Media;
 
 namespace Mvp.Feature.Navigation.Services
 {
   public class TopLinksBuilder : ITopLinksBuilder
   {
-    public IList<Link> GetTopLinks(Item contextItem, Rendering rendering)
-    {
-      var dataSourceItem = contextItem?.Database?.GetItem(rendering.DataSource);
-      if (dataSourceItem == null) throw new NullReferenceException();
-
-      return GetValidTopLinkItems(dataSourceItem).Select(c =>
-      {
-        var linkField = (LinkField) c.Fields[Templates.HasLink.Fields.Link];
-        var isMediaLink = linkField.IsMediaLink;
-        return new Link
+        public IList<Link> GetTopLinks(Item contextItem, Rendering rendering)
         {
-          Title = isMediaLink ? c.DisplayName : linkField.Title,
-          Url = isMediaLink ? linkField.GetFriendlyUrl() : linkField.Url
-        };
-      }).ToList();
-    }
+            var dataSourceItem = contextItem?.Database?.GetItem(rendering.DataSource);
+            if (dataSourceItem == null) throw new NullReferenceException();
+
+            return GetValidTopLinkItems(dataSourceItem).Select(c =>
+            {
+                var linkField = (LinkField)c.Fields[Templates.HasLink.Fields.Link];
+                var isMediaLink = linkField.IsMediaLink;
+                return new Link
+                {
+                    Title = isMediaLink ? c.DisplayName : linkField.Title,
+                    Url = isMediaLink ? MediaManager.GetMediaUrl(linkField.TargetItem, new MediaUrlBuilderOptions { AlwaysIncludeServerUrl = true }) : linkField.Url
+                };
+            }).ToList();
+        }
 
     private IEnumerable<Item> GetValidTopLinkItems(Item dataSourceItem)
     {
